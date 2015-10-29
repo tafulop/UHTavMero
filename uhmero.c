c9bc3a9993d6f825ba44231b6bf6f5c56d8ff9b0
tafulop <tamas.flp@gmail.com>
2015-10-29 20:04:38 +0100

    measuring works, displaying the results in cm

--- uhmero.c
+++ uhmero.c
@@ -9,9 +9,6 @@ void init_periph(); // pefifériák inicializálása
 void timer0_init();
 void impulse_start();
 void impulse_stop();
-void timer1_init();
-void timer1_stop();
-
 void timer2_init();
 void timer2_stop();
 
@@ -45,13 +42,9 @@ int main (void){
 
 ISR(TIMER2_COMP_vect)
 {	
-	blabla++;
+	blabla = blabla + 20;
 }
 
-/*
-ISR(TIMER2_OVF_vect){
-	led_out(6);
-}*/
 
 void init_periph(){
 
@@ -76,30 +69,21 @@ void timer0_init(){
 	TIMSK |= (1<<1);	// interrupt enable
 }
 
-void timer1_init(){
-	
-	//static unsigned int cnt = 0;
-
-	TCCR1B |= (1<<3);	// CTC mode, match on OCR1A
-	TIMSK |= (1<<4);
-	OCR1A = 7;
-}
-
 void set_uh_ports(){
 	DDRD |= 1;	// trigger kimenet
 	DDRD &= 0b11111101;	// echo bemenet
 }
 
 void ext_int_init(){
-	EICRA |= (1<<2)|(1<<3);
+	select_rising_edge();
 	EIMSK |= 2;
 }
 
 
 void impulse_start(){
-	//TCNT0 = 0;	// timer számláló regiszter nullázás
+	TCNT0 = 0;	// timer számláló regiszter nullázás
 	TCCR0 |= 1;	// timer 0 indítás
-//	PORTD |= 1; // trigger impulse 1
+	PORTD |= 1; // trigger impulse 1
 	led_out(1);
 }
 
@@ -121,20 +105,11 @@ void impulse_stop(){
 
 void measure_echo(){
 	
-	TCCR2 |= 1;	// timer 2 indítás
+	TCCR2 |= 3;	// timer 2 indítás
 	led_out(3);
 	
 }
 
-ISR(TIMER1_COMPA_vect){
-
-//	int* cnt = getCnt();
-//	*cnt = *cnt + 1;		// cnt increment cnt++
-//blabla++;
-	//static int lofasz = 1;
-//	led_out(0x55);
-
-}
 
 ISR(INT1_vect){
 	
@@ -142,16 +117,15 @@ ISR(INT1_vect){
 	EIFR = 0;
 
 	if(run%2){
-		select_falling_edge();
 		measure_echo();	
+		select_falling_edge();
 	}else{
 		timer2_stop();
-		select_rising_edge();
+		select_falling_edge();
 		calc_distance();	
 	}
 
-	run++;
-	
+	run++;	
 }
 
 void select_rising_edge(){
@@ -176,11 +150,6 @@ int* getCnt(){
 }
 
 
-void timer1_stop(){
-	TCCR1B &=  0xFE;	// clock source turned off
-	//TIMSK = 0;
-}
-
 void calc_distance(){
 	
 	//int* cnt = getCnt();
@@ -193,17 +162,16 @@ void calc_distance(){
 
 void timer2_init(){
 	
-	OCR2 = 7;
-//	TCCR2 |= (1<<3); // CTC mode 
+	OCR2 = 4;
+	TCCR2 |= (1<<3); // CTC mode 
 	TIMSK |= (1<<7);	// interrupt
 }
 
 void timer2_stop(){
-	//TIMSK &= ~(1<<7);	// interrupt
-	TIMSK = 0;
-	//led_out(4);
+	TIMSK &= ~(1<<7);	// interrupt
+	//TIMSK = 0;
+	led_out(4);
 }
-
 
 
 void print_result(){
