2ff88a4b20fb3d0e31ae3399fddc6e5c0e5071a7
tafulop <tamas.flp@gmail.com>
2015-10-29 18:43:15 +0100

    Measuring works but the measured value is wrong.

--- uhmero.c
+++ uhmero.c
@@ -23,7 +23,6 @@ void set_uh_ports();
 
 void measure_echo();
 void measure_echo_stop();
-
 unsigned int blabla = 0;
 
 int main (void){
@@ -31,19 +30,28 @@ int main (void){
 	
 
 	init_periph();
-	
+
 	
 	impulse_start();
 	
 
 	while(1){
-		//led_out(blabla);
-	
+		if(PING == 1){impulse_start();}
 	}	
 
 	return;
 }
 
+
+ISR(TIMER2_COMP_vect)
+{	
+	blabla++;
+}
+
+/*
+ISR(TIMER2_OVF_vect){
+	led_out(6);
+}*/
 
 void init_periph(){
 
@@ -62,10 +70,9 @@ void init_periph(){
 
 void timer0_init(){
 	
-	OCR0 = 79;
+	OCR0 = 159;
 	TCCR0 = 0;
 	TCCR0 |= (1<<3); // CTC mode 
-//	TCCR0 |= 7;
 	TIMSK |= (1<<1);	// interrupt enable
 }
 
@@ -84,7 +91,7 @@ void set_uh_ports(){
 }
 
 void ext_int_init(){
-	//EICRA |= (1<<2)|(1<<3);
+	EICRA |= (1<<2)|(1<<3);
 	EIMSK |= 2;
 }
 
@@ -92,21 +99,16 @@ void ext_int_init(){
 void impulse_start(){
 	//TCNT0 = 0;	// timer számláló regiszter nullázás
 	TCCR0 |= 1;	// timer 0 indítás
-	
-	PORTD |= 1; // trigger impulse 1
+//	PORTD |= 1; // trigger impulse 1
+	led_out(1);
 }
 
 
 ISR(TIMER0_COMP_vect)
 {	
 	impulse_stop();	// 10 usec impulzus vége
-	//led_out(1<<7);
-	led_out(1);
-	static int c =0;
-	//led_out(c++);
-	blabla++;
-	
-	measure_echo();	// mérés indítás
+	led_out(2);
+	//measure_echo();	// mérés indítás
 		
 }
 
@@ -114,14 +116,13 @@ void impulse_stop(){
 	//TCCR0 &= 0b11111110;
 	TIMSK &= 0b11111101;	// timer 0 CTC interrupt disable
 	PORTD &= 0xFE;	// trigger impulse 0
-	led_out(16);
+	//led_out(16);
 }
 
 void measure_echo(){
 	
-	//TCCR1B |= 1;	// start timer 1
 	TCCR2 |= 1;	// timer 2 indítás
-	//led_out(1);
+	led_out(3);
 	
 }
 
@@ -137,19 +138,30 @@ ISR(TIMER1_COMPA_vect){
 
 ISR(INT1_vect){
 	
-	//EIFR = 0;
-	EIMSK = 0;
+	static int run = 1;
+	EIFR = 0;
 
-	timer2_stop();
+	if(run%2){
+		select_falling_edge();
+		measure_echo();	
+	}else{
+		timer2_stop();
+		select_rising_edge();
+		calc_distance();	
+	}
 
-	//led_out(0xFF);
-	static int c =0;
-	//led_out(++c);
-	//calc_distance();
-	
+	run++;
 	
 }
 
+void select_rising_edge(){
+	EICRA |= (1<<2)|(1<<3);
+}
+
+void select_falling_edge(){
+	EICRA = 0;
+	EICRA |= (1<<3);
+}
 
 
 
@@ -165,38 +177,38 @@ int* getCnt(){
 
 
 void timer1_stop(){
-	//TCCR1B &=  0xFE;	// clock source turned off
-	TIMSK = 0;
+	TCCR1B &=  0xFE;	// clock source turned off
+	//TIMSK = 0;
 }
 
 void calc_distance(){
 	
-	int* cnt = getCnt();
-	int result = 0xff;//*cnt/58;
+	//int* cnt = getCnt();
+	//int result = 0xff;//*cnt/58;
 	//led_out(blabla);
 
-	lcd_put_int(result);
+	//led_out(6);
+	print_result();
 }
 
 void timer2_init(){
 	
 	OCR2 = 7;
-	TCCR2 |= (1<<3); // CTC mode 
+//	TCCR2 |= (1<<3); // CTC mode 
 	TIMSK |= (1<<7);	// interrupt
 }
 
 void timer2_stop(){
-	TIMSK = 0;	// interrupt
+	//TIMSK &= ~(1<<7);	// interrupt
+	TIMSK = 0;
+	//led_out(4);
 }
 
-ISR(TIMER2_COMP_vect)
-{	
-	//	int* cnt = getCnt();
-//	*cnt = *cnt + 1;		// cnt increment cnt++
-	blabla++;
-	//static int lofasz = 1;
-	//led_out(2);
-	//led_out(8);
+
+
+void print_result(){
+	
+	lcd_goto_yx(0,0);
+	lcd_put_int(blabla/58);
 
 }
-
